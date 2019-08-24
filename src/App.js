4f0687cac826b27c4bea60524c3bfcf2d5c3ee9e
a2626 <lucas091750@gmail.com>
2019-08-25 00:44:53 +1000

    Add basic router setting for hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
 import Homepage from './pages/homepage/homepage-componet'
 
+const HasPage = () =>(
+    <div>
+        <h1> Hats Page</h1>
+    </div>
+);
 function App() {
   return (
       <div>
-        <Homepage/>
+          <Switch>
+              <Route exact path='/' component={Homepage} />
+              <Route path='/hats' component={HasPage}/>
+
+
+          </Switch>
+
+
+
 
       </div>
 
