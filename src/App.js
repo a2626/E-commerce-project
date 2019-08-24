2abb1a736d0096e67c853b44055101e569377990
a2626 <lucas091750@gmail.com>
2019-08-24 18:55:59 +1000

    done the homepage design and menu items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage-componet'
+import Homepage from './pages/homepage/homepage-componet'
 
 function App() {
   return (
